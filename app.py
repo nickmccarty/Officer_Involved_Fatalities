0ae3d129b513f25cdfc7a6e2283bd43c958363e9
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2019-08-21 15:37:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from boto.s3.connection import S3Connection
+import os
+
+s3 = S3Connection(os.environ['API_KEY'], os.environ['API_KEY2'])
 
 app = Flask(__name__)
 
